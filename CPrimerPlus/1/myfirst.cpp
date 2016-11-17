f9d5703fcf84e7d4a44aab51e799a7e41c3d905b
qiang_wang_dev <wangqiang@cmos.com>
2016-11-17 15:22:52 +0800

    first example

---
+++ CPrimerPlus/1/myfirst.cpp
@@ -0,0 +1,18 @@
+/***********************************
+    > File Name: myfirst.cpp
+  > Author: wangqiang
+  > Mail: wangqiang@cmos.net
+  > Created Time: 2016年11月17日 星期四 15时16分39秒
+ **************************/
+
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+	cout<<"Come up dan C++ me some time";
+	cout<< endl;
+	cout<< "You won't regret it!"<<endl;
+	return 0;
+
+}
